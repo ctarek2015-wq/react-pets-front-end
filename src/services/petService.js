a8d75d656a33c9d299663d5e3b69d50131706c57
ahmed-tarek <c.tarek2015@gmail.com>
2026-09-06 16:05:00 +0300

    implement index function to fetch pet data from API

--- src/services/petService.js
+++ src/services/petService.js
@@ -1,1 +1,13 @@
 const BASE_URL = `${import.meta.env.VITE_BACKEND_SERVER_URL}/pets`;
+
+const index = async () => {
+  try {
+    const res = await fetch(BASE_URL);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export { index };
